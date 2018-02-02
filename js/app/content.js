43d90673bb59ff67daed764ff1718042600452da
unknown <cm10280@ISG-BR-CMOR.infsalesgroup.com>
2018-02-02 16:39:53 -0500

    Initial Commit

---
+++ js/app/content.js
@@ -0,0 +1,36 @@
+﻿//alert('content script loaded');
+//https://developer.chrome.com/apps/messaging
+chrome.extension.onMessage.addListener(
+function (request, sender, sendResponse) {
+
+    //debugger;
+    /*
+        - Retrieve Page Info 
+            - Title
+            - Season?
+            - Episode
+    */
+    if (request.action == 'PageInfo') {
+        var pageInfo = {
+            title : "NA",
+            episode : "NA"
+        };
+
+        try{
+            var title = $('meta[property="og:title"]').attr("content");
+            pageInfo.title = title;
+            
+            var episode = $('#showmedia_about_media h4:nth-child(2)');
+            if(episode){
+                episode = episode.text().trim();
+                pageInfo.episode = episode;    
+            }
+        }catch(exception){
+            console.log(exception)
+        }
+        
+        sendResponse(pageInfo);
+    }
+
+
+});
---
+++ js/app/popup.js
@@ -0,0 +1,49 @@
+﻿myApp.service('pageInfoService', function() {
+    this.getInfo = function(callback) {
+        chrome.tabs.query({'active': true},
+        function (tabs) {
+            if (tabs.length > 0)
+            {
+                chrome.tabs.sendMessage(tabs[0].id, { 'action': 'PageInfo' }, function (response) {
+                    console.log('response from sendMessage', response);
+                    callback(response);
+                });
+            }
+
+        });
+    };
+});
+
+myApp.service('ChromeStorage', function() {
+    
+    var save = function(pageInfo){
+        alert('Saving!', pageInfo);
+    }
+
+    return {
+        save : save
+    }
+
+});
+
+myApp.controller("PageController", function ($scope, pageInfoService, ChromeStorage) {
+    $scope.pageInfo = null;
+    $scope.init = function(){
+        //Load viewed animes
+        pageInfoService.getInfo(function (pageInfo) {
+            console.log('pageInfo', pageInfo);
+            $scope.pageInfo = pageInfo;
+            $scope.$apply();
+        });    
+    }
+
+    $scope.saveAnimeAsViewed = function(){
+        if($scope.pageInfo){
+            ChromeStorage.save($scope.pageInfo);
+        }
+    }
+    
+});
+
+
+
